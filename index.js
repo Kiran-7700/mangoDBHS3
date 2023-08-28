e5cd05bbf850261bae5f70fa658b52c0efe02bb6
Kiran-7700 <121047254+Kiran-7700@users.noreply.github.com>
2023-08-28 09:47:22 +0530

    done

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.get("/", (req, res) => {
     console.log("Api is running fine!")
 })
 
-app.listen(6000, async () => {
+app.listen(5001, async () => {
     try {
         await connect()
-        console.log("Server is running at port 6000")
+        console.log("Server is running at port 5001")
     }
     catch (err) {
         console.log(`Error in the server ${err}`)
